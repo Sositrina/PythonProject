874da311e0ad68e86334cd4577045635a4d51585
Vladislav <parovatkinvlad@gmail.com>
2025-10-09 18:04:32 +0300

    Тесты обновлены

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -5,21 +5,36 @@ from src.external_api import get_transaction_amount_in_rub
 # Тест USD → RUB
 with patch("src.external_api.requests.get") as mock_get:
     mock_get.return_value.json.return_value = {"rates": {"RUB": 100}}
-    transaction = {"amount": 10, "currency": "USD"}
-    result = get_transaction_amount_in_rub(transaction)
+    transaction_usd = {"operationAmount": {"amount": "10", "currency": {"code": "USD", "name": "доллар"}}}
+    result = get_transaction_amount_in_rub(transaction_usd)
     assert result == 1000
-    print("USD → RUB PASS")
+    print("USD → RUB")
 
 # Тест EUR → RUB
 with patch("src.external_api.requests.get") as mock_get:
     mock_get.return_value.json.return_value = {"rates": {"RUB": 120}}
-    transaction = {"amount": 5, "currency": "EUR"}
-    result = get_transaction_amount_in_rub(transaction)
+    transaction_eur = {"operationAmount": {"amount": "5", "currency": {"code": "EUR", "name": "евро"}}}
+    result = get_transaction_amount_in_rub(transaction_eur)
     assert result == 600
-    print("EUR → RUB PASS")
+    print("EUR → RUB")
 
 # Тест RUB → RUB
-transaction = {"amount": 500, "currency": "RUB"}
-result = get_transaction_amount_in_rub(transaction)
+transaction_rub = {"operationAmount": {"amount": "500", "currency": {"code": "RUB", "name": "руб."}}}
+result = get_transaction_amount_in_rub(transaction_rub)
 assert result == 500
-print("RUB → RUB PASS")
+print("RUB → RUB")
+
+
+# Неправильная структура транзации
+invalid_transaction = {"amount": "100"}
+try:
+    get_transaction_amount_in_rub(invalid_transaction)
+except ValueError as e:
+    print(e)
+
+# Другая валюта
+unsupported_currency = {"operationAmount": {"amount": "100", "currency": {"code": "GBP", "name": "фунт"}}}
+try:
+    get_transaction_amount_in_rub(unsupported_currency)
+except ValueError as e:
+    print(e)
